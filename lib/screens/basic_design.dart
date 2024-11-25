35d4453dcab69ef887b37023f31368ca04727ad3
Alvaro Antony Villanueva Barreto <barretoalvaro369@gmail.com>
2024-11-25 18:08:09 -0500

    Crear diseño básico parte 2.

--- lib/screens/basic_design.dart
+++ lib/screens/basic_design.dart
@@ -6,8 +6,45 @@ class BasicDesignScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Image(image: AssetImage('assets/landscape.jpg')),
+      body: Column(
+        children: [
+          Image(image: AssetImage('assets/landscape.jpg')),
+          // Título
+          Title()
+        ],
+      ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: Row(
+        children: [
+          const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Oeschinen Lake Campground',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Kandersteg, Switzerland',
+                style: TextStyle(color: Colors.black45),
+              ),
+            ],
+          ),
+          Expanded(child: Container()),
+          const Icon(Icons.star, color: Colors.red),
+          const Text('41')
+        ],
       ),
     );
   }
