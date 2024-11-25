48d7d47fa257043caf4e4840ac1dc18fbd82e369
Alvaro Antony Villanueva Barreto <barretoalvaro369@gmail.com>
2024-11-25 18:31:21 -0500

    Terminar diseño básico.

--- lib/screens/basic_design.dart
+++ lib/screens/basic_design.dart
@@ -5,12 +5,22 @@ class BasicDesignScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          Image(image: AssetImage('assets/landscape.jpg')),
+          const Image(image: AssetImage('assets/landscape.jpg')),
           // Título
-          Title()
+          const Title(),
+
+          // Button Section
+          const ButtonSection(),
+
+          // Description
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+            child: const Text(
+                'Duis ullamco et exercitation officia. Non officia culpa qui reprehenderit adipisicing irure proident proident officia pariatur veniam proident aute. Magna sit irure occaecat minim irure ut nostrud aute culpa. Exercitation reprehenderit laboris anim consectetur fugiat laboris dolore magna Lorem nulla.'),
+          )
         ],
       ),
     );
@@ -46,6 +56,66 @@ class Title extends StatelessWidget {
           const Text('41')
         ],
       ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: const Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          CustomButton(
+            icon: Icons.call,
+            text: 'Call',
+          ),
+          CustomButton(
+            icon: Icons.map_sharp,
+            text: 'Route',
+          ),
+          CustomButton(
+            icon: Icons.share,
+            text: 'Share',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  const CustomButton({
+    super.key,
+    required this.icon,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(
+          icon,
+          color: Colors.blue,
+          size: 30,
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Text(
+          text,
+          style: const TextStyle(color: Colors.blue),
+        )
+      ],
     );
   }
 }
