74c0fe5164946d517e2676aaa8850743f2759c40
Alvaro Antony Villanueva Barreto <barretoalvaro369@gmail.com>
2024-11-25 18:58:56 -0500

    Crear diseño con scroll parte 2.

--- lib/screens/scroll_design.dart
+++ lib/screens/scroll_design.dart
@@ -9,9 +9,46 @@ class ScrollDesingScreen extends StatelessWidget {
         body: Stack(
       children: [
         // Background Image
-        Background()
+        Background(),
+        // Main content
+        MainContent()
       ],
     ));
+  }
+}
+
+class MainContent extends StatelessWidget {
+  const MainContent({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    const textStyle = TextStyle(
+        fontSize: 50, fontWeight: FontWeight.bold, color: Colors.white);
+
+    return SafeArea(
+      bottom: false,
+      child: Column(
+        children: [
+          const SizedBox(height: 30),
+          const Text(
+            '11°',
+            style: textStyle,
+          ),
+          const Text(
+            'Miércoles',
+            style: textStyle,
+          ),
+          Expanded(child: Container()),
+          const Icon(
+            Icons.keyboard_arrow_down,
+            size: 100,
+            color: Colors.white,
+          ),
+        ],
+      ),
+    );
   }
 }
 
