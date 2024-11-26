c123f226604fb27242f32f4cb63f0192e3763453
Alvaro Antony Villanueva Barreto <barretoalvaro369@gmail.com>
2024-11-26 00:28:24 -0500

    Refactorizar código y terminar app.

--- lib/widgets/card_table.dart
+++ lib/widgets/card_table.dart
@@ -80,6 +80,38 @@ class _SingleCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return _CardBackground(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CircleAvatar(
+          backgroundColor: color,
+          radius: 30,
+          child: Icon(
+            icon,
+            size: 35,
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Text(
+          text,
+          style: TextStyle(color: color, fontSize: 18),
+        )
+      ],
+    ));
+  }
+}
+
+class _CardBackground extends StatelessWidget {
+  final Widget child;
+
+  const _CardBackground({required this.child});
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(15),
       child: ClipRRect(
@@ -92,27 +124,7 @@ class _SingleCard extends StatelessWidget {
               color: const Color.fromRGBO(62, 66, 107, 0.7),
               borderRadius: BorderRadius.circular(20),
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  backgroundColor: color,
-                  radius: 30,
-                  child: Icon(
-                    icon,
-                    size: 35,
-                    color: Colors.white,
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  text,
-                  style: TextStyle(color: color, fontSize: 18),
-                )
-              ],
-            ),
+            child: child,
           ),
         ),
       ),
